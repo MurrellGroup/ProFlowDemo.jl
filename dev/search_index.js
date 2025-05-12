2a4a28dd3f1d2e37d4a06f95c443a0af9073acd8
Documenter.jl <documenter@juliadocs.github.io>
2025-05-12 12:16:42 +0000

    build based on ce295b0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ProFlowDemo","page":"Home","title":"ProFlowDemo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ProFlowDemo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
